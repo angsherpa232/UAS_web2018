795db1dda419fb07ae35a443c618af2992ea40e9
unknown <danji_ma90@hotmail.com>
2018-05-13 15:12:17 +0200

    updating express module

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ const path = require('path')
 const PORT = process.env.PORT || 5000
 
 express()
-  .use(express.static(path.join(__dirname, 'public')))
+  .use(express.static(path.join(__dirname, '/public')))
   .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'html')
-  .get('/', (req, res) => res.render('index'))
+  //.set('view engine', 'html')
+  .get('/', (req, res) => res.render('index.html'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
