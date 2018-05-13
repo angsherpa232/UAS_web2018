3de6eeddf1b5e48b0d94401edf5b9209bcc24bd4
unknown <danji_ma90@hotmail.com>
2018-05-13 15:28:31 +0200

    still testing express server

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
+var app = express()
 
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/index.html'));
