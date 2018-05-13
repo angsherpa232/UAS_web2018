2b0c81ad30a0d3d8124ee16c35ae5b21c720a34b
unknown <danji_ma90@hotmail.com>
2018-05-13 13:00:32 +0200

    created views folder...

--- index.js
+++ index.js
@@ -5,6 +5,5 @@ const PORT = process.env.PORT || 5000
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
+  .get('/', (req, res) => res.render('index'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
