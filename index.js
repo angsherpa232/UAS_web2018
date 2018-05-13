77e274799d11f772098f0f60ad3ebf74059f2992
unknown <danji_ma90@hotmail.com>
2018-05-13 17:03:17 +0200

    html scripts not loading when called

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
-const express = require('express')
-const path = require('path')
+var express = require('express');
+var app = express();
 const port = process.env.PORT || 5000
-var app = express()
+const path = require('path')
 
 console.log("Starting server")
 
+// Allow cross origin
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+app.set('port', port);
+
 app.get('/', function(req, res) {
   console.log("starting request")
-  res.sendFile(path.join(__dirname + '/index.html'));
+  res.sendFile(path.join(__dirname + '/views/index.html'));
 });
 
 app.listen(port, function() {
