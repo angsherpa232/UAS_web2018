b4936a40c320c03969590aa42edc3228f288bf48
unknown <danji_ma90@hotmail.com>
2018-05-13 15:08:06 +0200

    added view engine to index.js

--- index.js
+++ index.js
@@ -5,5 +5,6 @@ const PORT = process.env.PORT || 5000
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'html')
   .get('/', (req, res) => res.render('index'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
