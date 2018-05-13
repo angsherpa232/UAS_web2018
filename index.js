484263b64d19829f5dfd345d7f3f0ead6eff07f6
unknown <danji_ma90@hotmail.com>
2018-05-13 15:24:52 +0200

    still testing express server

--- index.js
+++ index.js
@@ -2,9 +2,16 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
-express()
-  .use(express.static(path.join(__dirname, '/public')))
-  .set('views', path.join(__dirname, 'views'))
-  //.set('view engine', 'html')
-  .get('/', (req, res) => res.render('index.html'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/index.html'));
+});
+
+app.listen(8080);
+
+
+// express()
+//   .use(express.static(path.join(__dirname, '/index.html')))
+//   .set('views', path.join(__dirname, 'views'))
+//   //.set('view engine', 'html')
+//   .get('/', (req, res) => res.render('index.html'))
+//   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
