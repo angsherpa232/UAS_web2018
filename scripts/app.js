283d185d9c6ff6d6063cbdb05127845a614c46d0
angsherpa232 <angsherpa232@gmail.com>
2018-07-04 10:25:23 +0200

    working processing as expected except glitches in unit of measurement

--- scripts/app.js
+++ scripts/app.js
@@ -570,153 +570,86 @@ angular.module('UAS_2018', [
 }])
 
 //Process controllers
-.controller('uas2018_process_controller', ['$scope', '$http', function($scope, $http) {
+.controller('uas2018_process_controller', ['$scope', function($scope) {
+////Load map when the processing tab is opened for the first time /////
+var imagery = L.esri.basemapLayer("Imagery");
+var geojsonLayer;
+// Main map object
+var map = L.map('map', {
+  center: [51.944990, 7.572810],
+  zoom: 17,
+  layers: [imagery],
+  maxZoom: 21,
+  maxNativeZoom: 19
+});
 
-//Processing begins
-// initialize wpsService
-// var wpsService = new WpsService({
-//   url: "http://geoprocessing.demo.52north.org:8080/wps/WebProcessingService",
-//   version: "2.0.0"
-// });
-//
-// var capabilities,data,jobId,
-//   processDescription; // the process description
-//
-//
-//   var capabilitiesCallback = function(response) {
-//
-//     var capabilities = response;
-//     var processes = response.capabilities.processes;
-//     var processCapabilities = response.responseDocument;
-//     console.log(capabilities.capabilities.processes[3]);
-//     console.log(capabilities.responseDocument);
-//
-//   };
-//
-//   var describeProcessCallback = function(response) {
-//
-//     processDescription = response;
-//
-//     //set value of textarea
-//     var processDocument = processDescription.responseDocument;
-//     var xml = new XMLSerializer().serializeToString(processDocument);
-//     console.log(processDocument);
-// // console.log(xml);
-//   };
-//
-// // wpsService.getResult_WPS_2_0(resultCallback, '888e6472-cad7-4245-8124-72e04b45bf68');
-// // <!-- wpsService.getResult_WPS_2_0(resultCallback,'f7fdb7b3-9f4d-4abb-8495-3964e5694842'); -->
-//
-// //Execute process callback function
-// var executeCallback = function (response){
-// 	var status = response.executeResponse.responseDocument.status;
-// 	if (status == "Accepted"){
-// 	jobId = response.executeResponse.responseDocument.jobId;
-// 	return jobId;
-// 	}
-// jobId = response.executeResponse.responseDocument.jobId;
-// return jobId;
-// };
-//
-// setTimeout(function(){wpsService.getStatus_WPS_2_0(statusCallback, jobId)},1150);
-//
-// var resultCallback = function(response){
-//   // console.log(response);
-//   data = response.executeResponse.responseDocument.outputs[0].data.complexData.value;
-//   console.log(data);
-//   return data;
-// }
-//
-// var statusCallback = function(response){
-//   <!-- var jobId = response.executeResponse.responseDocument.jobId; -->
-//   var status = response.executeResponse.responseDocument.status;
-//   if (status == "Succeeded"){
-// 	wpsService.getResult_WPS_2_0(resultCallback,jobId);
-//   }
-//   else {
-// 	alert('Oops! something went wrong, please try again.');
-//   }
-// }
-//
-// /////////////////// INPUT //////////////////////
-// //Input generator for literal dataType
-// var inputGenerator = new InputGenerator();
-// //Literal input
-// // var literalInput = inputGenerator.createLiteralDataInput_wps_1_0_and_2_0(identifier, dataType, uom, value);
-// var literalInput = inputGenerator.createLiteralDataInput_wps_1_0_and_2_0('width', undefined, 'meter', 0.003);
-//
-// //Input generator for complex dataType
-// //Complex dataType
-// // var complexInput = inputGenerator.createComplexDataInput_wps_1_0_and_2_0(identifier,
-// // 					mimeType, schema, encoding, asReference, complexPayload);
-// var complexInput = inputGenerator.createComplexDataInput_wps_1_0_and_2_0('data',
-// 					'application/vnd.geo+json', undefined, undefined, true, 'https://api.myjson.com/bins/ccsyq');
-//
-//
-//
-// ////////////////// OUTPUT /////////////////////
-// //Output generator for complex dataType
-// var outputGenerator = new OutputGenerator();
-//
-// // var complexOutput = outputGenerator.createComplexOutput_WPS_2_0(identifier, mimeType, schema,
-// // 			encoding, transmission);
-// var complexOutput = outputGenerator.createComplexOutput_WPS_2_0('result', 'application/vnd.geo+json', undefined,
-// 			undefined, 'value');
-//
-//
-// // wpsService.getCapabilities_GET(capabilitiesCallback);
-// // wpsService.describeProcess_GET(describeProcessCallback,'org.n52.wps.server.algorithm.SimpleBufferAlgorithm');
-//
-// //WPS execute
-// // wpsService.execute(callbackFunction, processIdentifier, responseFormat, executionMode, lineage, inputs, outputs);
-// wpsService.execute(executeCallback, 'org.n52.wps.server.algorithm.SimpleBufferAlgorithm', 'document', 'async', false, [literalInput,complexInput], [complexOutput]);
-//Processing end
+var mapHome = {
+  lat: 51.944990,
+  lng: 7.572810,
+  zoom: 17
+};
+
+// Default base layers when the app initiates
+var baseLayers = {
+  "Imagery": imagery
+};
+
+/// Holding the buffer layers ///////
+  var assetLayerGroup = L.layerGroup().addTo(map);
+
+//// Retrieving geojson file begins ///////
+var dataURL = "./home/resources/demo_3Points.geojson"
+$.ajax({
+  url: dataURL,
+  async: false,
+  success: function(response) {
+      demoPoints = L.geoJson(response).addTo(map);
+      map.fitBounds(demoPoints.getBounds());
+  }
+}).responseJSON
+
+////// Map loading session ends here ////////
+
 
 /// Buffer input begins //////
-$scope.buffer_radius = '';
 $scope.bufferFunction = function () {
-    console.log("The input value has changed. The new value is:"+$scope._value);
     if ($scope.buffer_radius == ''){
       $scope._value='btn btn-primary disabled';
     } else {
       $scope._value='btn btn-primary enabled';
     }
 }
+
+
 /// Buffer input end /////
 
-/// Execute buffer ///
+/// Execute buffer begins///
 $scope.executeBuffer = function () {
   if ($scope.buffer_radius != ''){
-    console.log($scope.buffer_radius);
-    // initialize wpsService
+//// Execute buffer ends /////
+
+  /////// wps service begin /////////
     var wpsService = new WpsService({
       url: "http://geoprocessing.demo.52north.org:8080/wps/WebProcessingService",
       version: "2.0.0"
     });
 
-    var capabilities,data,jobId,
-      processDescription; // the process description
+    var capabilities,data_,jobId,
+      processDescription;
 
 
       var capabilitiesCallback = function(response) {
-
         var capabilities = response;
         var processes = response.capabilities.processes;
-        var processCapabilities = response.responseDocument;
         console.log(capabilities.capabilities.processes[3]);
-        console.log(capabilities.responseDocument);
-
       };
 
       var describeProcessCallback = function(response) {
-
         processDescription = response;
-
         //set value of textarea
         var processDocument = processDescription.responseDocument;
         var xml = new XMLSerializer().serializeToString(processDocument);
         console.log(processDocument);
-    // console.log(xml);
       };
 
     // wpsService.getResult_WPS_2_0(resultCallback, '888e6472-cad7-4245-8124-72e04b45bf68');
@@ -735,12 +668,25 @@ $scope.executeBuffer = function () {
 
     setTimeout(function(){wpsService.getStatus_WPS_2_0(statusCallback, jobId)},1350);
 
+////// $scope.plot first parse the callback returned string and then plot ///////
+    $scope.plot = function(d){
+      console.log('the geojson is '+geojsonLayer);
+              geojsonLayer = L.geoJson(JSON.parse(d)).addTo(assetLayerGroup);
+              map.fitBounds(geojsonLayer.getBounds());
+  }
+
+  $scope.clearBuffer = function (){
+    assetLayerGroup.clearLayers();
+  }
+
     var resultCallback = function(response){
       // console.log(response);
-      data = response.executeResponse.responseDocument.outputs[0].data.complexData.value;
-      console.log(data);
-      return data;
+      data_ = response.executeResponse.responseDocument.outputs[0].data.complexData.value;
+  ///////// $scope.plot the callback returned object to map /////////
+      $scope.plot(data_);
     }
+
+
 
     var statusCallback = function(response){
       <!-- var jobId = response.executeResponse.responseDocument.jobId; -->
@@ -758,15 +704,14 @@ $scope.executeBuffer = function () {
     var inputGenerator = new InputGenerator();
     //Literal input
     // var literalInput = inputGenerator.createLiteralDataInput_wps_1_0_and_2_0(identifier, dataType, uom, value);
-    var literalInput = inputGenerator.createLiteralDataInput_wps_1_0_and_2_0('width', undefined, 'meter', 0.003);
+    var literalInput = inputGenerator.createLiteralDataInput_wps_1_0_and_2_0('width', undefined, 'meters', $scope.buffer_radius);
 
     //Input generator for complex dataType
     //Complex dataType
     // var complexInput = inputGenerator.createComplexDataInput_wps_1_0_and_2_0(identifier,
     // 					mimeType, schema, encoding, asReference, complexPayload);
     var complexInput = inputGenerator.createComplexDataInput_wps_1_0_and_2_0('data',
-    					'application/vnd.geo+json', undefined, undefined, true, 'https://api.myjson.com/bins/ccsyq');
-
+    					'application/vnd.geo+json', undefined, undefined, true, 'https://api.myjson.com/bins/oj3ag');
 
 
     ////////////////// OUTPUT /////////////////////
@@ -787,59 +732,7 @@ $scope.executeBuffer = function () {
     wpsService.execute(executeCallback, 'org.n52.wps.server.algorithm.SimpleBufferAlgorithm', 'document', 'async', false, [literalInput,complexInput], [complexOutput]);
   };
 }
-console.log($scope.buffer_radius);
-/// Execute buffer ///
-  // Load basemaps
-  var topo = L.esri.basemapLayer("Imagery");
-
-  // Main map object
-  var map = L.map('map', {
-    center: [51.944990, 7.572810],
-    zoom: 17,
-    layers: [topo],
-    maxZoom: 19,
-    maxNativeZoom: 19
-  });
-
-  var mapHome = {
-    lat: 51.944990,
-    lng: 7.572810,
-    zoom: 17
-  };
-
-  // Default base layers when the app initiates
-  var baseLayers = {
-    "Topographic": topo
-  };
-
-  //// Retrieving geojson file begins ///////
-  var dataURL = "./home/resources/result1.geojson"
-
-  $.ajax({
-    url: dataURL,
-    async: false,
-    success: function(response) {
-      geojsonLayer = L.geoJson(response).addTo(map);
-      map.fitBounds(geojsonLayer.getBounds());
-    }
-  }).responseJSON
-
-  var data = './home/resources/markers_project.geojson';
-  $.ajax({
-    url: data,
-    async: false,
-    success: function(response) {
-      geojsonLayer = L.geoJson(response).addTo(map);
-      map.fitBounds(geojsonLayer.getBounds());
-    }
-  }).responseJSON
-
-  //// Retrieving geojson file ends ///////
-
-// Basemap
-
-//Basemap
-
+/////// wps service ends /////////
 }])
 
 
