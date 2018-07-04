9e11855e508e02c2f64c04a91d5970f1f757c7cb
angsherpa232 <angsherpa232@gmail.com>
2018-07-04 19:40:33 +0200

    processing controller added

--- scripts/app.js
+++ scripts/app.js
@@ -36,6 +36,7 @@ angular.module('UAS_2018', [
       })
 
       .when('/processing', {
+        controller: 'uas2018_process_controller',
         templateUrl: './home/views/processing.html'
       })
 
