652436be3ff7a3fb9e50ebc0aef7955685ac1717
danji90 <danji_ma90@hotmail.com>
2018-09-11 16:15:13 +0200

    cleaned and commented serverd.js

--- serverd.js
+++ serverd.js
@@ -1,21 +1,18 @@
+// Declare variables and constants
 var express = require('express');
 var app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5000;   //Define port
 const path = require('path');
-const keys = require('./config/keys');
-var bodyParser = require("body-parser");
-app.use(bodyParser.json({type: 'application/json'}));
+const keys = require('./config/keys');  //imports username and password from keys.js
+var bodyParser = require("body-parser");   //body-parser package necessary for JSON parsing
 
-var bodyParser = require("body-parser");
-app.use(bodyParser.json());
+app.use(bodyParser.json({type: 'application/json'}));
 
 console.log("Starting server");
 
+// Declare static folders for the server
 app.use(express.static(path.join(__dirname + '/modules')));
-//app.use(express.static(path.join(__dirname + '/css')));
-//app.use(express.static(path.join(__dirname + '/scripts')));
 app.use(express.static(path.join(__dirname + '/node_modules')));
-//app.use(express.static(path.join(__dirname + '/process_resources')));
 
 // Allow cross origin
 app.use(function(req, res, next) {
@@ -24,30 +21,34 @@ app.use(function(req, res, next) {
   next();
 });
 
+// Set port
 app.set('port', port);
 
-//View engine for rendering html
+// View engine for rendering html
 var engine = require('consolidate');
 app.engine('html', engine.mustache);
 app.set('view engine', 'html');
 
+// Request app rendering
 app.get('/', function(req, res) {
   console.log("starting request")
   res.render(path.join(__dirname + '/index.html'));
 });
 
-
+// Declare authentication variables (import from keys.js)
 var loginName = keys.users.username;
 var loginPassword = keys.users.password;
 
+// Check if user input data corresponds to credentials
 app.post('/login', function(req, res, next){
-
   if (loginName == req.body.username && loginPassword == req.body.password){
+    // Render if they correspond
     res.render(path.join(__dirname + '/index.html'));
   }else{
   }
 })
 
+// Print if server is listening
 app.listen(port, function() {
   console.log("You've been served!")
   console.log("Node app is running at localhost:" + app.get('port'))
