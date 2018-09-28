7432f1db858195d5d0444d871587c9545feb9b8e
angsherpa232 <angsherpa232@gmail.com>
2018-09-28 13:33:43 +0200

    upgrade to ES6

--- serverd.js
+++ serverd.js
@@ -1,14 +1,11 @@
 // Declare variables and constants
-var express = require('express');
-var app = express();
-const port = process.env.PORT || 5000;   //Define port
+const express = require('express');
+const app = express();
 const path = require('path');
 const keys = require('./config/keys');  //imports username and password from keys.js
-var bodyParser = require("body-parser");   //body-parser package necessary for JSON parsing
+const bodyParser = require("body-parser");   //body-parser package necessary for JSON parsing
 
 app.use(bodyParser.json({type: 'application/json'}));
-
-console.log("Starting server");
 
 // Declare static folders for the server
 app.use(express.static(path.join(__dirname + '/modules')));
@@ -21,9 +18,6 @@ app.use(function(req, res, next) {
   next();
 });
 
-// Set port
-app.set('port', port);
-
 // View engine for rendering html
 var engine = require('consolidate');
 app.engine('html', engine.mustache);
@@ -31,16 +25,15 @@ app.set('view engine', 'html');
 
 // Request app rendering
 app.get('/', function(req, res) {
-  console.log("starting request")
   res.render(path.join(__dirname + '/index.html'));
 });
 
 // Declare authentication variables (import from keys.js)
-var loginName = keys.users.username;
-var loginPassword = keys.users.password;
+const loginName = keys.users.username;
+const loginPassword = keys.users.password;
 
 // Check if user input data corresponds to credentials
-app.post('/login', function(req, res, next){
+app.post('/login', (req, res)=>{
   if (loginName == req.body.username && loginPassword == req.body.password){
     // Render if they correspond
     res.render(path.join(__dirname + '/index.html'));
@@ -48,8 +41,10 @@ app.post('/login', function(req, res, next){
   }
 })
 
+//Define port
+const port = process.env.PORT || 5000;   
+
 // Print if server is listening
-app.listen(port, function() {
-  console.log("You've been served!")
-  console.log("Node app is running at localhost:" + app.get('port'))
+app.listen(port, ()=> {
+  console.log(`Node app is running at port ${port}`)
 });
